71f00dc04f785c262870ee460b9f7578b36fb4cd
szapp <dev.szapp@gmail.com>
2023-07-31 11:00:23 +0200

    Make compatible with 1.12f and 1.30 fix

--- Ninja/NoUnderwater/Content/NinjaInit.d
+++ Ninja/NoUnderwater/Content/NinjaInit.d
@@ -4,10 +4,15 @@
 func void Ninja_NoUnderwater_Init() {
     MEM_InitAll();
 
-    const int zCAICamera__CheckUnderWaterFX_G1 = 4932160; //0x4B4240
-    const int zCAICamera__CheckUnderWaterFX_G2 = 4969184; //0x4BD2E0
+    const int zCAICamera__CheckUnderWaterFX_G1C = 4932160; //0x4B4240
+    const int zCAICamera__CheckUnderWaterFX_G1A = 4993120; //0x4C3060
+    const int zCAICamera__CheckUnderWaterFX_G2C = 4959600; //0x4BAD70
+    const int zCAICamera__CheckUnderWaterFX_G2A = 4969184; //0x4BD2E0
 
-    var int addr; addr = MEMINT_SwitchG1G2(zCAICamera__CheckUnderWaterFX_G1, zCAICamera__CheckUnderWaterFX_G2);
+    var int addr; addr = MEMINT_SwitchExe(zCAICamera__CheckUnderWaterFX_G1C,
+                                          zCAICamera__CheckUnderWaterFX_G1A,
+                                          zCAICamera__CheckUnderWaterFX_G2C,
+                                          zCAICamera__CheckUnderWaterFX_G2A);
 
     if (MEM_ReadByte(addr) != ASMINT_OP_retn) {
         // Just immediately jump out of the setter function
