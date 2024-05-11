04d2c75055ce809df6ad368c45bd8fc395253e6b
szapp <dev.szapp@gmail.com>
2024-05-11 18:20:12 +0200

    Fix syntax error

--- Ninja/NoUnderwater/Content/init.d
+++ Ninja/NoUnderwater/Content/init.d
@@ -4,7 +4,7 @@
 func void Ninja_NoUnderwater_Init() {
     MEM_InitAll();
 
-    const int zCAICamera__CheckUnderWaterFX = { /*G1*/4932160, /*G1A*/4993120, /*G2*/4959600, /*G2A*/4969184 };
+    const int zCAICamera__CheckUnderWaterFX[4] = { /*G1*/4932160, /*G1A*/4993120, /*G2*/4959600, /*G2A*/4969184 };
     var int addr; addr = zCAICamera__CheckUnderWaterFX[NoUnderwater_EXE];
 
     if (MEM_ReadByte(addr) != ASMINT_OP_retn) {
